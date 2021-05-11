92ac99d83b33e9146de65f64ebbeb4607a6e1f64
Oleksandr Bondarchuk <al.v.bondarchuk@gmail.com>
2021-05-11 23:10:08 +0200

    implemented CoordinatesValidator

---
+++ src/Tests/Unit/ToyRobot.Domain.UnitTests/CoordinatesValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ToyRobot.Domain.UnitTests
+{
+    class CoordinatesValidatorTests
+    {
+        private CoordinatesValidator _sut;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _sut = new CoordinatesValidator();
+        }
+
+        [TestCase(5, 5, 5, 0)]
+        [TestCase(5, 5, 0, 5)]
+        [TestCase(3, 3, -1, 0)]
+        [TestCase(3, 3, 0, -1)]
+        public void ShouldThrow_ForCoordinatesThatAreOutsideTheMap(int mapDimensionX, int mapDimensionY, int coordinateX, int coordinateY)
+        {
+            MapDimensions mapDimensions = new MapDimensions(mapDimensionX, mapDimensionY);
+            Coordinates invalidCoordinates = new Coordinates(coordinateX, coordinateY);
+
+            Assert.Throws<ArgumentException>(() => _sut.ValidateAndThrow(mapDimensions, invalidCoordinates));
+        }
+
+        [TestCase(5, 5, 4, 0)]
+        [TestCase(5, 5, 0, 4)]
+        [TestCase(3, 3, 0, 0)]
+        [TestCase(3, 3, 1, 1)]
+        public void ShouldNotThrow_ForCoordinatesThatAreInTheMapBoundaries(int mapDimensionX, int mapDimensionY, int coordinateX, int coordinateY)
+        {
+            MapDimensions mapDimensions = new MapDimensions(mapDimensionX, mapDimensionY);
+            Coordinates invalidCoordinates = new Coordinates(coordinateX, coordinateY);
+
+            Assert.DoesNotThrow(() => _sut.ValidateAndThrow(mapDimensions, invalidCoordinates));
+        }
+    }
+}
--- src/ToyRobot.Domain/CoordinatesValidator.cs
+++ src/ToyRobot.Domain/CoordinatesValidator.cs
@@ -4,6 +4,11 @@ namespace ToyRobot.Domain
 {
     interface ICoordinatesValidator
     {
+        /// <summary>
+        /// The SOUTH WEST most corner of the map considered to have (0,0) coordinates
+        /// </summary>
+        /// <param name="mapDimensions">Map size</param>
+        /// <param name="coordinates">Coordinates that need to be validated</param>
         public void ValidateAndThrow(MapDimensions mapDimensions, Coordinates coordinates);
     }
 
@@ -11,7 +16,16 @@ namespace ToyRobot.Domain
     {
         public void ValidateAndThrow(MapDimensions mapDimensions, Coordinates coordinates)
         {
-            throw new NotImplementedException();
+            const int originX = 0;
+            const int originY = 0;
+
+            bool xCoordinateIsOutsideTheMap = coordinates.X < originX || coordinates.X >= mapDimensions.X;
+            bool yCoordinateIsOutsideTheMap = coordinates.Y < originY || coordinates.Y >= mapDimensions.Y;
+
+            if (xCoordinateIsOutsideTheMap || yCoordinateIsOutsideTheMap)
+            {
+                throw new ArgumentException($"Given coordinates [{coordinates.X}, {coordinates.Y}] are outside of the map with [{mapDimensions.X}, {mapDimensions.Y}] size");
+            }
         }
     }
 }
